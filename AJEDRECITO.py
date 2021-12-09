375aa4ddf01301accf2de3737aacb701e750d5d3
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-09 17:38:47 +0100

    estoesimposible.py

--- AJEDRECITO.py
+++ AJEDRECITO.py
@@ -29,6 +29,41 @@ ALFIL_NEGRO = chr(0x265D)
 CABALLO_NEGRO = chr(0x265E)
 PEÓN_NEGRO = chr(0x265F)
 
+#posicioninicialpeonesblancos
+PB1y = 6
+PB1x = 0
+(tablero[PB1y])[PB1x] = PEÓN_BLANCO
+
+def mov_peonblanco1(PB1y, PB1x, tablero):
+    pb1 = input("A DÓNDE DESEAS MOVER EL PEON BLANCO?")
+
+mov_peonblanco1(PB1y, PB1x, tablero)
+
+(tablero[6])[1] = PEÓN_BLANCO
+(tablero[6])[2] = PEÓN_BLANCO
+(tablero[6])[3] = PEÓN_BLANCO
+(tablero[6])[4] = PEÓN_BLANCO
+(tablero[6])[5] = PEÓN_BLANCO
+(tablero[6])[6] = PEÓN_BLANCO
+(tablero[6])[7] = PEÓN_BLANCO
+
+
+#posicioninicialpeonesnegros
+(tablero[1])[0] = PEÓN_NEGRO
+(tablero[1])[1] = PEÓN_NEGRO
+(tablero[1])[2] = PEÓN_NEGRO
+(tablero[1])[3] = PEÓN_NEGRO
+(tablero[1])[4] = PEÓN_NEGRO
+(tablero[1])[5] = PEÓN_NEGRO
+(tablero[1])[6] = PEÓN_NEGRO
+(tablero[1])[7] = PEÓN_NEGRO
+
+
+
+
+
+
+
 
 
 fichero = input("ESCOGE UN NOMBRE PARA EL FICHERO>>>>")
