ddaf254c98132581040c5e5e0912a8f6a522fed0
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-09 20:33:27 +0100

    commit

--- AJEDRECITO.py
+++ AJEDRECITO.py
@@ -1,5 +1,8 @@
+from os import supports_effective_ids, terminal_size
+
+
 tablero =[
-    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [" ",' ',' ',' ',' ',' ',' ',' '],
     [' ',' ',' ',' ',' ',' ',' ',' '],
     [' ',' ',' ',' ',' ',' ',' ',' '],
     [' ',' ',' ',' ',' ',' ',' ',' '],
@@ -29,23 +32,47 @@ ALFIL_NEGRO = chr(0x265D)
 CABALLO_NEGRO = chr(0x265E)
 PEÓN_NEGRO = chr(0x265F)
 
+a = 0
+b = 1
+c = 2
+d = 3
+e = 4
+f = 5
+g = 6
+h = 7
+
+uno = 7
+dos = 6
+tres = 5
+cuatro = 4
+cinco = 3
+seis = 2
+siete = 1
+ocho = 0
+
+printeartablero(tablero)
+
 #posicioninicialpeonesblancos
-PB1y = 6
-PB1x = 0
-(tablero[PB1y])[PB1x] = PEÓN_BLANCO
 
-def mov_peonblanco1(PB1y, PB1x, tablero):
-    pb1 = input("A DÓNDE DESEAS MOVER EL PEON BLANCO?")
+#def moverunapieza(tablero,a,b,c,d,e,f,g,h,uno,dos,tres,cuatro,cinco,seis,siete,ocho):
+    #fila = input("fila donde ESTÁ LA PIEZA QUE DESEAS MOVER >>> ")
+    #columna = input("columna donde esta la pieza que quieres mover")
+    #(tablero[fila])[columna] = " "
+    #printeartablero(tablero)
 
-mov_peonblanco1(PB1y, PB1x, tablero)
 
-(tablero[6])[1] = PEÓN_BLANCO
-(tablero[6])[2] = PEÓN_BLANCO
-(tablero[6])[3] = PEÓN_BLANCO
-(tablero[6])[4] = PEÓN_BLANCO
-(tablero[6])[5] = PEÓN_BLANCO
-(tablero[6])[6] = PEÓN_BLANCO
-(tablero[6])[7] = PEÓN_BLANCO
+
+
+
+
+(tablero[dos])[a] = PEÓN_BLANCO
+(tablero[dos])[b] = PEÓN_BLANCO
+(tablero[dos])[c] = PEÓN_BLANCO
+(tablero[dos])[d] = PEÓN_BLANCO
+(tablero[dos])[e] = PEÓN_BLANCO
+(tablero[dos])[f] = PEÓN_BLANCO
+(tablero[dos])[g] = PEÓN_BLANCO
+(tablero[dos])[h] = PEÓN_BLANCO
 
 
 #posicioninicialpeonesnegros
@@ -58,11 +85,47 @@ mov_peonblanco1(PB1y, PB1x, tablero)
 (tablero[1])[6] = PEÓN_NEGRO
 (tablero[1])[7] = PEÓN_NEGRO
 
+printeartablero(tablero)
 
 
+#while True:
+    #inicio = input("Elige la fila y la columna que desea mover")
+    #inicio = inicio.split()
+    #if len(inicio) == 2:
+        #filaI = inicio[0]
+        #columnaI = inicio[1]
+        #try:
+            #filaI = int(filaI)
+            #columnaI = int(columnaI)
+        #except:
+            #pass
+        #else: 
+            #if filaI >= 0 and filaI < 8 and columnaI >= 0 and columnaI < 8:
+                #break
+
+#while True:
+    #final = input("Elige la fila y la columna a la que desea ir")
+    #final = final.split()
+    #if len(final) == 2:
+        #filaF = final[0]
+        #columnaF = final[1]
+        #try:
+            #filaF = int(filaF)
+            #columnaF = int(columnaI)
+        #except:
+            #pass
+        #else:
+            #if filaF >= 0 and filaF < 8 and columnaF >= 0 and columnaF < 8 and final != inicio:
+                #(tablero[filaF])[columnaF] = tablero[filaI][columnaI]
+                #(tablero[filaI][columnaI]) = " "
+                #break
 
 
-
+#def moverunapieza(tablero,a,b,c,d,e,f,g,h,uno,dos,tres,cuatro,cinco,seis,siete,ocho):
+    #fila = input("fila donde ESTÁ LA PIEZA QUE DESEAS MOVER >>> ")
+    #columna = input("columna donde esta la pieza que quieres mover")
+    #(tablero[fila])[columna] = " "
+    #printeartablero(tablero)
 
 
 
@@ -72,3 +135,4 @@ f = open(fichero, "a+", encoding="utf-8")
 
 
 printeartablero(tablero)
+moverunapieza(tablero,a,b,c,d,e,f,g,h,uno,dos,tres,cuatro,cinco,seis,siete,ocho)
